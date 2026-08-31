5c2d042ed85c35f3bcd0c3857c54821e701c78fd
skrovk <33180646+skrovk@users.noreply.github.com>
2026-08-31 15:57:41 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequency_map = {}
+
+        for num in nums:
+            if frequency_map.get(num) is None:
+                frequency_map[num] = 1
+                continue
+            
+            frequency_map[num] += 1
+
+
+        buckets = [[] for i in range(len(nums) + 1)]
+
+        for num, freq in frequency_map.items():
+            buckets[freq].append(num)
+
+        solution = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                solution.append(num)
+
+                if len(solution) == k:
+                    return solution
